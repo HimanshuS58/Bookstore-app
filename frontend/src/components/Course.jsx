2545aaef20cde92212f688dc8047d18a61394428
Himanshu Singh <142159346+HimanshuS58@users.noreply.github.com>
2026-03-05 04:35:02 +0530

    Update book API endpoint to production URL

--- frontend/src/components/Course.jsx
+++ frontend/src/components/Course.jsx
@@ -14,7 +14,7 @@ const Course = () => {
 
         const getBook = async () => {
             try {
-                const res = await axios.get('http://localhost:4001/book');
+                const res = await axios.get('https://bookstore-app-backend-tp91.onrender.com');
                 console.log(res.data)
                 setBook(res.data)
             } catch(error) {
