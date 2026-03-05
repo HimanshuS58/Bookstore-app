6e16b23566904d16c968910ad775737ceb49bb03
Himanshu Singh <142159346+HimanshuS58@users.noreply.github.com>
2026-03-05 18:44:11 +0530

    Update login API endpoint URL

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -15,7 +15,7 @@ const Login = () => {
             password: data.password
         }
 
-        await axios.post('https://bookstore-app-backend-tp91.onrender.com/user/login', userInfo)
+        await axios.post('https://bookstore-app-backend-xxf2.onrender.com/user/login', userInfo)
         .then((res)=> {
             console.log(res.data);
             if(res.data){
